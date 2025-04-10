efe336c76217234e85f3381c415465801c887669
Brenton Lian <blian@unc.edu>
2025-04-10 19:13:55 -0400

    Half of files pasted

---
+++ Todoey Twoey/Todoey Twoey/ContentView.swift
@@ -0,0 +1,71 @@
+//
+//  ContentView.swift
+//  Todoey Twoey
+//
+//  Created by Brenton on 4/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject private var vm = TodoListViewModel()
+    @State private var newTodoTitle: String = ""
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                Section {
+                    TextField("Title", text: $newTodoTitle)
+                        .onSubmit {
+                            Task {
+                                await vm.createTodo(title: newTodoTitle)
+                                newTodoTitle = ""
+                            }
+                        }
+                }
+                
+                switch vm.state {
+                case .idle: Text("Make a request")
+                case .loading: Text("Loading...")
+                case .success(let todos): todoListView(todos: todos)
+                case .error(let message): Text("Error: \(message)")
+                }
+            }
+            .navigationTitle("Todoey Twoey")
+            .refreshable {
+                await vm.fetchTodos()
+            }
+        }
+        .task {
+            await vm.fetchTodos()
+        }
+    }
+    
+    @ViewBuilder
+    private func todoListView(todos: [Todo]) -> some View {
+        ForEach(todos) { todo in
+            Button {
+                Task {
+                    await vm.toggleCompletion(for: todo)
+                }
+            } label: {
+                Label(todo.title, systemImage: todo.isCompleted ? "circle.fill" : "circle")
+            }
+            .swipeActions {
+                Button(role: .destructive) {
+                    Task {
+                        await vm.delete(todo: todo)
+                    }
+                } label: {
+                    Image(systemName: "trash")
+                }
+                .tint(.red)
+            }
+        }
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
